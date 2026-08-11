4f46594cc63567a9cf39029b88dcc7f797ddb188
Nikolas Martens <nikolas.m@rtens.org>
2026-08-11 16:55:34 +0200

    Illegal moves: pinned checker

--- test/test.cpp
+++ test/test.cpp
@@ -609,6 +609,13 @@ void illegalMoves() {
 		g.restore("///4r///4R/4K");
 		should(pm(g.moves(52)), ",e2e3,e2e4,e2e5");
 	});
+
+	test("pinned checker", []() {
+		Game g;
+		g.restore("8/8/3k2bR/8/8/P2K4/8/8 w");
+		should(pm(g.moves(43)), ",d3c4,d3d4,d3c3,d3e3,d3d2,d3e2");
+		should(pm(g.moves(40)), "");
+	});
 }
 
 int main() {
